c2728b4af7955d751d3393e7a9bd2231e881a44b
vdunlop <148563390+vdunlop@users.noreply.github.com>
2023-11-24 18:49:38 -0600

    Replies displaying correctly. Clear main screen and question screens working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-const MAX_QUIZ_TIME = 5; // quiz timer in seconds
+// Clearing the screen elements set up. 
+var startContainerEl = document.querySelector(".startContainer");
+var questionContainerEl = document.querySelector(".questionContainer");
+
+// Timer values
+const MAX_QUIZ_TIME = 10; // quiz timer in seconds
 var secondsLeft = MAX_QUIZ_TIME;
 
 // Buttons we will listen for.
@@ -8,69 +13,185 @@ var answerBBtn = document.querySelector("#reply2");
 var answerCBtn = document.querySelector("#reply3");
 var answerDBtn = document.querySelector("#reply4");
 
+// Upper right corner timer display
 var timeLeftDisplayEl = document.getElementById("#timeLeftDisplay");
 var initialTimeLeftDisplay = timeLeftDisplayEl.textContent;
+
+// The quiz is stored here;
+const NUMBER_OF_QUESTIONS = 5;
+var currentQuestion = 0;
+var questions = ["Java Question 1",
+  "Which of the following is NOT a Falsy",
+  "Java Question 3",
+  "Java Question 4",
+  "Java Question 5"];
+  var answerChoices = [["strings", "booleans", "alerts", "numbers"],
+                      ["null","undefined","\"false\"", "-0"],
+                    ["a1","a2","a3","a4"],
+                    ["1","2","3","4"],
+                    ["answer1","answer2","answer3","answer4"]];
+  var correctAnswers = ["A", "D", "C", "A", "B"];
 
 // This function displays the current time remaining on the 
 // quiz timer (time left).
 function displayTimeRemaining() {
- //   debugger;
- //   console.log("time remaining " + secondsLeft);
- //   console.log("element " + timeLeftDisplayEl);
-    timeLeftDisplayEl.textContent = initialTimeLeftDisplay + secondsLeft;
+  //   debugger;
+  //   console.log("time remaining " + secondsLeft);
+  //   console.log("element " + timeLeftDisplayEl);
+  timeLeftDisplayEl.textContent = initialTimeLeftDisplay + secondsLeft;
 }
 
 // This function starts the quiz timer, monitors for it to run out, and
 // displays time remaining.
 function startTestTimer() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
- 
-      if(secondsLeft <= 0) {
-        console.log ("timer ran out");
-        // Timer ran out
-        clearInterval(timerInterval);
-        // Clear Timer display - set to zero
-        timeLeftDisplayEl.textContent = initialTimeLeftDisplay + secondsLeft;
-      } else {
-        // Continue quiz
-        // Display current time remaining
-        displayTimeRemaining();
-      }  
-  
-    }, 1000);
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+
+    if (secondsLeft <= 0) {
+      console.log("timer ran out");
+      // Timer ran out
+      clearInterval(timerInterval);
+      // Clear Timer display - set to zero
+      timeLeftDisplayEl.textContent = initialTimeLeftDisplay + secondsLeft;
+      allDoneDisplay();
+    } else {
+      // Continue quiz
+      // Display current time remaining
+      displayTimeRemaining();
+      //processQuestion();
+    }
+
+  }, 1000);
 }
 
 // This section of functions will process answers A through D and determine
 // whether they are correct or not
-function processAnswerA(){
+function processAnswerA() {
   console.log("Answer A");
+  console.log("current question " + currentQuestion);
+  console.log(questions[currentQuestion]);
+  currentQuestion++;
+  if (currentQuestion >= NUMBER_OF_QUESTIONS) {
+    allDoneDisplay();
+  } else {
+    console.log("processing A not all done");
+
+    clearScreen();
+    processQuestion();
+  }
 }
-function processAnswerB(){
+
+function processAnswerB() {
   console.log("Answer B");
+  console.log("current question " + currentQuestion);
+  console.log(questions[currentQuestion]);
+  currentQuestion++;
+  if (currentQuestion >= NUMBER_OF_QUESTIONS) {
+    allDoneDisplay();
+  } else {
+    console.log("processing B not all done");
+    clearScreen();
+    processQuestion();
+  }
 }
-function processAnswerC(){
+function processAnswerC() {
   console.log("Answer C");
+  console.log("current question " + currentQuestion);
+  console.log(questions[currentQuestion]);
+  currentQuestion++;
+  if (currentQuestion >= NUMBER_OF_QUESTIONS) {
+    allDoneDisplay();
+  } else {
+    console.log("processing C not all done");
+    clearScreen();
+    processQuestion();
+  }
 }
-function processAnswerD(){
+function processAnswerD() {
   console.log("Answer D");
+  console.log("current question " + currentQuestion);
+  console.log(questions[currentQuestion]);
+  currentQuestion++;
+  if (currentQuestion >= NUMBER_OF_QUESTIONS) {
+    allDoneDisplay();
+  } else {
+    console.log("processing D not all done");
+    clearScreen();
+    processQuestion();
+  }
 }
 
-// Add event listener here for the Start Quiz button click
-startQuizBtn.addEventListener("click",function() {
-    startTestTimer();
-    // 
-}) 
+function clearScreen() {
+  //debugger;
+  console.log("CLEAR SCREEN");
+  console.log("current question " + currentQuestion);
+startContainerEl.setAttribute("hidden","true");
+element = document.getElementById('clearContainer');
+if (element.style.display == 'none') {
+  element.style.display = 'block';
+}
+questionContainerEl.setAttribute("hidden", "false");
 
-answerABtn.addEventListener("click",function() {
+//.setAttribute('style','display:none;');
+//document.getElementsByClassName('startContainerOn').style.display = 'none';
+}
+
+// Display the question on the screen.
+function processQuestion() {
+
+  if (currentQuestion >= NUMBER_OF_QUESTIONS) {
+    console.log("complete");
+  } else {
+    console.log("not complete " + currentQuestion);
+    console.log(" PROCESS NEXT QUESTION");
+    console.log("displaying question " + currentQuestion);
+    console.log("display question" + questions[currentQuestion]);
+    //debugger;
+    element = document.getElementById("reply1");
+    element.textContent = answerChoices[currentQuestion][0];
+    element = document.getElementById("reply2");
+    element.textContent = answerChoices[currentQuestion][1];
+    element = document.getElementById("reply3");
+    element.textContent = answerChoices[currentQuestion][2];
+    element = document.getElementById("reply4");
+    element.textContent = answerChoices[currentQuestion][3];
+
+    currentQuestion++;
+  }
+}
+
+function allDoneDisplay() {
+  console.log("ALL DONE");
+}
+
+// Event listener for the Start Quiz button click
+startQuizBtn.addEventListener("click", function (event) {
+  event.stopPropagation;
+  secondsLeft = MAX_QUIZ_TIME; // Reset the timer.
+  currentQuestion = 0;  // Reset the current question.
+  //debugger;
+  startContainerEl.setAttribute("hidden","false"); // main screen is visible
+  questionContainerEl.setAttribute("hidden", "true"); // questions are hidden
+  clearScreen();
+  processQuestion();
+  startTestTimer();
+  // 
+})
+
+// Event listenters for answer buttons clicked.
+answerABtn.addEventListener("click", function (event) {
+  event.stopPropagation();
   processAnswerA();
 })
-answerBBtn.addEventListener("click",function() {
+answerBBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
   processAnswerB();
 })
-answerCBtn.addEventListener("click",function() {
+answerCBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
   processAnswerC();
 })
-answerDBtn.addEventListener("click",function() {
+answerDBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
   processAnswerD();
 })
